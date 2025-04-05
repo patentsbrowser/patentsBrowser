1cc56fa4764d2d8e2002205dfc8d72ac3a786913
sriharsh1103 <prashant.sriharsh@antiersolutions.com>
2025-04-05 18:04:07 +0530

    feature:stage build completed

--- frontend/src/Components/Authentication/Login.tsx
+++ frontend/src/Components/Authentication/Login.tsx
@@ -24,7 +24,6 @@ const Login = ({ switchToSignup }: { switchToSignup: () => void }) => {
     },
     onSuccess: (data) => {
       console.log("Login response:", data);
-      console.log("Login response:");
 
 
       if (data.statusCode === 200) {
