06c9953ffaa8c09fceb83c75d24d74c3b9a6997b
patentsbrowser <patentsbrowser@gmail.com>
2025-04-24 18:34:59 +0530

    fix: redner issue solve

--- backend/src/routes/patentNormalizationRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { normalizePatents } from '../controllers/patentNormalizationController';
-
-const router = express.Router();
-
-router.post('/normalize', normalizePatents);
-
-export default router; 
